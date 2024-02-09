d826ea2726c79da4982160d947dc42aec111f3b1
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-02-09 19:25:02 +0000

    Added form and post request to add movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,26 @@
 class MoviesController < ApplicationController
+  def create
+    new_movie = Movie.new
+
+    new_movie.title = params["title"]
+    new_movie.year = params["year"]
+    new_movie.duration = params["duration"]
+    new_movie.description = params["description"]
+    new_movie.image = params["image"]
+    new_movie.director_id = params["director_id"]
+
+    new_movie.save
+
+    redirect_to("/movies")
+    # Retrieve the user's input from params
+    # Create a record in the movie table
+    # Populate each column with the user's input
+    # Save
+
+    # Redirect the user to /movies URL
+    
+  end
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
